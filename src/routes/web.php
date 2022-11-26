19d847c52a4177248708744d8ec3e0cbc45dd6bc
manaki-e <48690114s@gmail.com>
2022-11-26 16:19:27 +0900

    Laravel breeze install

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\ChoiceController;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,15 @@ Route::get('/quiz_list', function () {
 });
 
 Route::get('/quiz/{id?}', [QuizController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
