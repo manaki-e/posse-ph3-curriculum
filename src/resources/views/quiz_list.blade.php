5b867eec6c6beec12dbe8ebb5a232cc3c1ae731b
manaki-e <48690114s@gmail.com>
2023-01-01 13:16:06 +0900

    quiz_listをトップページとして表示

--- src/resources/views/quiz_list.blade.php
+++ src/resources/views/quiz_list.blade.php
@@ -1,23 +1,22 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <main>
+@extends('layouts.user')
+
+@section('content')
     <ul>
-      <li>
-      ガチで東京の人しか解けない！
-      <a href="quiz/1">#東京の難読地名クイズ</a>
-      </li>
-      <li>
-      広島県民なら解けて当然？
-      <a href="quiz/2">#広島県の難読地名クイズ</a>
-      </li>
+        <li class="flex gap-2 justify-between mb-1">
+            <div class="py-2 px-4">ガチで東京の人しか解けない！</div>
+            <a href="quiz/1">
+                <button class="bg-blue-600 font-semibold text-white py-2 px-4 rounded">
+                    #東京の難読地名クイズ
+                </button>
+            </a>
+        </li>
+        <li class="flex gap-2 justify-between">
+            <div class="py-2 px-4">広島県民なら解けて当然？</div>
+            <a href="quiz/2">
+                <button class="bg-blue-600 font-semibold text-white py-2 px-4 rounded">
+                    #広島の難読地名クイズ
+                </button>
+            </a>
+        </li>
     </ul>
-  </main>
-</body>
-</html>
+@endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/quiz_list', function () {
+Route::get('/', function () {
     return view('quiz_list');
 });
 
